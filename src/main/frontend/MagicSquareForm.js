7e086d2d8b9d2d9a1c2b2bf2413a40c116880961
Anton Karpov <89912213+sojicute@users.noreply.github.com>
2022-05-11 10:22:26 +0400

    Update MagicSquareForm.js
    Update label descrption

--- src/main/frontend/MagicSquareForm.js
+++ src/main/frontend/MagicSquareForm.js
@@ -10,17 +10,17 @@ export function MagicSquareForm({input, onInputChange}) {
                 </p>
                 <h6>Например:</h6>
 
-                <p>1 2 3 4 5 6 7 8 9
+                <p>123456789
                     Превращается в
 
-                    4 3 8 2 7 6 9 5 1
+                    438276951
 
                     за стоимость равную 26.
                 </p>
             </div>
 
             <div className="field">
-                <label className="label">Введите цифры от уникальные цифры 1 до 9</label>
+                <label className="label">Введите уникальные цифры от 1 до 9</label>
                 <div className="control">
                     <input onChange={onInputChange} className="input" type="text" value={input} placeholder="123456789"/>
                 </div>
